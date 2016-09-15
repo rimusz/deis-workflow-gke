65c760c74b9b50bd6f6064a3d2e5f91f7bf35c73
Rimantas Mocevicius <rmocius@gmail.com>
2016-09-15 11:47:49 +0100

    initial commit

---
+++ install_workflow_2_gke.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+
+install() {
+  # get k8s cluster name
+  cluster
+
+  # get lastest macOS helmc cli version
+  install_helmc
+
+  # get lastest macOS deis cli version
+  install_deis
+
+  # add Deis Chart repo
+  echo "Adding Deis Chart repository ... "
+  helmc repo add deis https://github.com/deis/charts
+  # get the latest version of all Charts from all repos
+  echo " "
+  echo "Get the latest version of all Charts from all repos ... "
+  helmc up
+
+  # get latest Workflow version
+  echo " "
+  echo "Getting latest Deis Workflow version ..."
+  WORKFLOW_RELEASE=$(ls ~/.helmc/cache/deis | grep workflow-v2. | grep -v -e2e | sort -rn | head -1 | cut -d'-' -f2)
+  echo "Got Deis Workflow ${WORKFLOW_RELEASE} ..."
+
+  # delete the old folder if such exists
+  rm -rf ~/.helmc/workspace/charts/workflow-${WORKFLOW_RELEASE}-${K8S_NAME} > /dev/null 2>&1
+
+  # fetch Deis Workflow Chart to your helmc's working directory
+  echo " "
+  echo "Fetching Deis Workflow Chart to your helmc's working directory ..."
+  helmc fetch deis/workflow-${WORKFLOW_RELEASE} workflow-${WORKFLOW_RELEASE}-${K8S_NAME}
+
+  ####
+  # set env vars
+  # so we do not have to edit generate_params.toml in chart’s tpl folder
+  # set storage to GCS
+  STORAGE_TYPE=gcs
+  GCS_KEY_JSON=$(cat service_account_key.json)
+  GCS_REGISTRY_BUCKET=${K8S_NAME}-deis-registry
+  GCS_DATABASE_BUCKET=${K8S_NAME}-deis-database
+  GCS_BUILDER_BUCKET=${K8S_NAME}-deis-builder
+  # set off-cluster registry
+  DEIS_REGISTRY_LOCATION=gcr
+  GCR_KEY_JSON=$(cat service_account_key.json)
+  if [[ "$1" == "eu" ]]
+  then
+    GCR_HOSTNAME="eu.gcr.io"
+  else
+    GCR_HOSTNAME=""
+  fi
+  #
+  export STORAGE_TYPE GCS_KEY_JSON GCS_REGISTRY_BUCKET GCS_DATABASE_BUCKET GCS_BUILDER_BUCKET DEIS_REGISTRY_LOCATION GCR_KEY_JSON GCR_HOSTNAME
+  ###
+  if [[ "$1" == "eu" ]]
+  then
+    # create GCS buckets in EU region
+    echo " "
+    echo "Creating GCS buckets in EU region..."
+    gsutil mb -l eu gs://${GCS_REGISTRY_BUCKET}
+    gsutil mb -l eu gs://${GCS_DATABASE_BUCKET}
+    gsutil mb -l eu gs://${GCS_BUILDER_BUCKET}
+  fi
+  ####
+
+  # generate manifests
+  echo " "
+  echo "Generating Workflow ${WORKFLOW_RELEASE}-${K8S_NAME} manifests ..."
+  helmc generate -x manifests -f workflow-${WORKFLOW_RELEASE}-${K8S_NAME}
+
+  # install Workflow
+  echo " "
+  echo "Installing Workflow ..."
+  helmc install workflow-${WORKFLOW_RELEASE}-${K8S_NAME}
+
+  # Waiting for Deis Workflow to be ready
+  wait_for_workflow
+  #
+
+  # get router's external IP
+  echo " "
+  echo "Fetching Router's LB external IP:"
+  LB_IP=$(kubectl --namespace=deis get svc | grep [d]eis-router | awk '{ print $3 }')
+  echo "$LB_IP"
+
+  echo " "
+  echo "Workflow install ${WORKFLOW_RELEASE} is done ..."
+  echo " "
+}
+
+upgrade() {
+  # get k8s cluster name
+  cluster
+
+  # get lastest macOS helmc cli version
+  install_helmc
+
+  # get lastest macOS deis cli version
+  install_deis
+
+  # get the latest version of all Charts from all repos
+  echo " "
+  echo "Get the latest version of all Charts from all repos ... "
+  helmc up
+  echo " "
+
+  # Fetch the current database credentials
+  echo " "
+  echo "Fetching the current database credentials ..."
+  kubectl --namespace=deis get secret database-creds -o yaml > ~/tmp/active-deis-database-secret-creds.yaml
+
+  # Fetch the builder component ssh keys
+  echo " "
+  echo "Fetching the builder component ssh keys ..."
+  kubectl --namespace=deis get secret builder-ssh-private-keys -o yaml > ~/tmp/active-deis-builder-secret-ssh-private-keys.yaml
+
+  # export environment variables for the previous and latest Workflow versions
+  export PREVIOUS_WORKFLOW_RELEASE=$(cat ~/tmp/active-deis-builder-secret-ssh-private-keys.yaml | grep chart.helm.sh/version: | awk '{ print $2 }')
+  export DESIRED_WORKFLOW_RELEASE=$(ls ~/.helmc/cache/deis | grep workflow-v2. | grep -v -e2e | sort -rn | head -1 | cut -d'-' -f2)
+
+  # delete the old chart folder if such exists
+  rm -rf ~/.helmc/workspace/charts/workflow-${DESIRED_WORKFLOW_RELEASE}-${K8S_NAME} > /dev/null 2>&1
+
+  # Fetching the new chart copy from the chart cache into the helmc workspace for customization
+  echo " "
+  echo "Fetching Deis Workflow Chart to your helmc's working directory ..."
+  helmc fetch deis/workflow-${DESIRED_WORKFLOW_RELEASE} workflow-${DESIRED_WORKFLOW_RELEASE}-${K8S_NAME}
+
+  ####
+  # set env vars
+  # so we do not have to edit generate_params.toml in chart’s tpl folder
+  # set storage to GCS
+  STORAGE_TYPE=gcs
+  GCS_KEY_JSON=$(cat service_account_key.json)
+  GCS_REGISTRY_BUCKET=${K8S_NAME}-deis-registry
+  GCS_DATABASE_BUCKET=${K8S_NAME}-deis-database
+  GCS_BUILDER_BUCKET=${K8S_NAME}-deis-builder
+  # set off-cluster registry
+  DEIS_REGISTRY_LOCATION=gcr
+  GCR_KEY_JSON=$(cat service_account_key.json)
+  if [[ "$2" == "eu" ]]
+  then
+    GCR_HOSTNAME="eu.gcr.io"
+  else
+    GCR_HOSTNAME=""
+  fi
+  #
+  export STORAGE_TYPE GCS_KEY_JSON GCS_REGISTRY_BUCKET GCS_DATABASE_BUCKET GCS_BUILDER_BUCKET DEIS_REGISTRY_LOCATION GCR_KEY_JSON GCR_HOSTNAME
+  ####
+
+  # Generate templates for the new release
+  echo " "
+  echo "Generating Workflow ${DESIRED_WORKFLOW_RELEASE}-${K8S_NAME} manifests ..."
+  helmc generate -x manifests workflow-${DESIRED_WORKFLOW_RELEASE}-${K8S_NAME}
+
+  # Copy your active database secrets into the helmc workspace for the desired version
+  cp -f ~/tmp/active-deis-database-secret-creds.yaml \
+    $(helmc home)/workspace/charts/workflow-${DESIRED_WORKFLOW_RELEASE}-${K8S_NAME}/manifests/deis-database-secret-creds.yaml
+
+  # Copy your active builder ssh keys into the helmc workspace for the desired version
+  cp -f ~/tmp/active-deis-builder-secret-ssh-private-keys.yaml \
+    $(helmc home)/workspace/charts/workflow-${DESIRED_WORKFLOW_RELEASE}-${K8S_NAME}/manifests/deis-builder-secret-ssh-private-keys.yaml
+
+  # Uninstall Workflow
+  echo " "
+  echo "Uninstalling Workflow ${PREVIOUS_WORKFLOW_RELEASE} ... "
+  helmc uninstall workflow-${PREVIOUS_WORKFLOW_RELEASE}-${K8S_NAME} -n deis
+
+  sleep 3
+
+  # Install of latest Workflow release
+  echo " "
+  echo "Installing Workflow ${DESIRED_WORKFLOW_RELEASE} ... "
+  helmc install workflow-${DESIRED_WORKFLOW_RELEASE}-${K8S_NAME}
+
+  # Waiting for Deis Workflow to be ready
+  wait_for_workflow
+
+  echo " "
+  echo "Workflow upgrade to ${DESIRED_WORKFLOW_RELEASE} is done ..."
+  echo " "
+
+}
+
+cluster() {
+  # get k8s cluster name
+  echo " "
+  echo "Fetching GKE cluster name ..."
+  K8S_NAME=$(kubectl config current-context)
+  echo "GKE cluster name is ${K8S_NAME} ..."
+  echo " "
+}
+
+install_deis() {
+  # get lastest macOS deis cli version
+  echo "Downloading latest version of Workflow deis cli for macOS"
+  curl -o ~/bin/deis https://storage.googleapis.com/workflow-cli/deis-latest-darwin-amd64
+  chmod +x ~/bin/deis
+  echo " "
+  echo "Installed latest deis cli to ~/bin ..."
+  echo " "
+}
+
+install_helmc() {
+  # get lastest macOS helmc cli version
+  echo "Downloading latest version of helmc cli for macOS"
+  curl -o ~/bin/helmc https://storage.googleapis.com/helm-classic/helmc-latest-darwin-amd64
+  chmod +x ~/bin/helmc
+  echo " "
+  echo "Installed latest helmc cli to ~/bin ..."
+  echo " "
+}
+
+wait_for_workflow() {
+  echo " "
+  echo "Waiting for Deis Workflow to be ready... but first, coffee! "
+  spin='-\|/'
+  i=1
+  until kubectl --namespace=deis get po | grep [d]eis-builder- | grep "1/1"  >/dev/null 2>&1; do i=$(( (i+1) %4 )); printf "\r${spin:$i:1}"; sleep .1; done
+  until kubectl --namespace=deis get po | grep [d]eis-registry- | grep "1/1"  >/dev/null 2>&1; do i=$(( (i+1) %4 )); printf "\r${spin:$i:1}"; sleep .1; done
+  until kubectl --namespace=deis get po | grep [d]eis-database- | grep "1/1"  >/dev/null 2>&1; do i=$(( (i+1) %4 )); printf "\r${spin:$i:1}"; sleep .1; done
+  until kubectl --namespace=deis get po | grep [d]eis-registry- | grep "1/1"  >/dev/null 2>&1; do i=$(( (i+1) %4 )); printf "\r${spin:$i:1}"; sleep .1; done
+  until kubectl --namespace=deis get po | grep [d]eis-router- | grep "1/1"  >/dev/null 2>&1; do i=$(( (i+1) %4 )); printf "\r${spin:$i:1}"; sleep .1; done
+  until kubectl --namespace=deis get po | grep [d]eis-controller- | grep "1/1"  >/dev/null 2>&1; do i=$(( (i+1) %4 )); printf "\r${spin:$i:1}"; sleep .1; done
+  echo " "
+}
+
+usage() {
+    echo "Usage: install_workflow_2_gke.sh install [eu] | upgrade [eu] | deis | helmc | cluster"
+}
+
+case "$1" in
+        install)
+                install $2
+                ;;
+        upgrade)
+                upgrade $2
+                ;;
+        deis)
+                install_deis
+                ;;
+        helmc)
+                install_helmc
+                ;;
+        cluster)
+                cluster
+                ;;
+        *)
+                usage
+                ;;
+esac
---
+++ readme.md
@@ -0,0 +1,39 @@
+Install Deis Workflow on to GKE without fuss 
+========================
+
+It allows to install [Deis Workflow PaaS](https://deis.com/workflow/) on to GKE with persistent [Object Storage](https://deis.com/docs/workflow/installing-workflow/configuring-object-storage/) set to Google Cloud Storage and Registry to [gcr.io](https://cloud.google.com/container-registry/)
+
+How to install
+----------
+
+
+Clone repository:
+
+```
+$ git clone https://github.com/rimusz/deis-workflow-gke
+```
+
+How it works
+------------
+
+```
+$ ./install_workflow_2_gke.sh
+Usage: install_workflow_2_gke.sh install [eu] | upgrade [eu] | deis | helmc | cluster
+```
+
+You will be able:
+
+- [install](https://deis.com/docs/workflow/installing-workflow/) : by defautl set Object Storage US region, use `eu` flag for EU region
+- [upgrade](https://deis.com/docs/workflow/managing-workflow/upgrading-workflow/) : upgrades to the latest Workflow version (use the same region as was for install)
+- deis - fetches the latest Workflow `deis` cli
+- helmc - fetches the latest [Helm Classic](https://github.com/helm/helm-classic) cli
+- cluster - shows cluster GKE name
+
+### have fun!
+
+
+## Contributing
+
+`install_workflow_2_gke.sh` is an [open source](http://opensource.org/osd) project release under
+the [Apache License, Version 2.0](http://opensource.org/licenses/Apache-2.0),
+hence contributions and suggestions are gladly welcomed! 
